d5f78101ca38361adda45020dab6d0e9904ffcb3
SamDav0 <150506020+SamDav0@users.noreply.github.com>
2025-04-17 14:49:17 +0530

    Update pc_repo.java

--- PC-Builder_Forum/src/main/java/com/example/demo/repositories/pc_repo.java
+++ PC-Builder_Forum/src/main/java/com/example/demo/repositories/pc_repo.java
@@ -1,10 +1,17 @@
 package com.example.demo.repositories;
 
 import com.example.demo.bases.Pc;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface pc_repo extends JpaRepository<Pc,Long>{
-    
+public interface pc_repo extends JpaRepository<Pc, Long> {
+
+    @Query("SELECT p FROM Pc p WHERE p.User.id = :userId")
+    List<Pc> findAllByUserId(@Param("userId") int userId);
 }
